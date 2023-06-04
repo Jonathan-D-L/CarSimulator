7d63ea522d11f5b63454392e7934cf43912a4a63
Jonathan.Donath.Lopez <Jonathan.Donath.Lopez@hotmail.com>
2023-06-04 19:00:47 +0200

    added tests -crashchancetest -textspacertest

---
+++ CarSimulator.Tests/Utilities/CarCrashChanceTests.cs
@@ -0,0 +1,35 @@
+﻿using UtilityLibrary;
+
+namespace CarSimulator.Tests.Utilities;
+
+[TestClass]
+public class CarCrashChanceTests
+{
+
+    [TestMethod]
+    public void CrashChance_Threshold_0()
+    {
+        var crashThreshold = 0;
+        var result = CarCrashChance.CrashChance(crashThreshold);
+        Assert.IsFalse(result);
+    }
+    [TestMethod]
+    public void CrashChance_Threshold_Above_0()
+    {
+        var crashThreshold = 1;
+        var results = new List<bool>();
+        for (var i = 0; i < 100; i++)
+        {
+            var result = CarCrashChance.CrashChance(crashThreshold);
+            results.Add(result);
+        }
+        Assert.IsTrue(results.Any(r => r));
+    }
+    [TestMethod]
+    public void CrashChance_Threshold_100()
+    {
+        var crashThreshold = 100;
+        var result = CarCrashChance.CrashChance(crashThreshold);
+        Assert.IsTrue(result);
+    }
+}
---
+++ CarSimulator.Tests/Utilities/TextSpacerTests.cs
@@ -0,0 +1,28 @@
+﻿using UtilityLibrary;
+
+namespace CarSimulator.Tests.Utilities;
+
+
+
+[TestClass]
+public class TextSpacerTests
+{
+    [TestMethod]
+    public void TextSpace_Returns_Correct_Spacing_Int()
+    {
+        var spaceAmount = 10;
+        var subtractAmount = 9;
+        var expectedResult = spaceAmount - subtractAmount;
+        var result = TextSpacer.SpaceText(spaceAmount, subtractAmount);
+        Assert.AreEqual(expectedResult, result.Length);
+    }
+    [TestMethod]
+    public void TextSpace_Returns_Correct_Spacing_String()
+    {
+        var spaceAmount = 10;
+        var subtractAmount = "NINE TEST";
+        var expectedResult = spaceAmount - subtractAmount.Length;
+        var result = TextSpacer.SpaceText(spaceAmount, subtractAmount);
+        Assert.AreEqual(expectedResult, result.Length);
+    }
+}
